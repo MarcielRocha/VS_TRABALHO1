47202b3544b9968ac6d88e11c38d2a8bc59292e1
MarcielRocha <103228009+MarcielRocha@users.noreply.github.com>
2023-05-27 15:21:09 -0300

    Add files via upload

---
+++ TRABALHOS/BLC/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(char.ToUpper('c'));             // C
+Console.WriteLine(char.IsWhiteSpace('\t'));       // True
+Console.WriteLine(char.IsLetter('x'));            // True
+Console.WriteLine(char.GetUnicodeCategory('x'));  // LowercaseLetter
