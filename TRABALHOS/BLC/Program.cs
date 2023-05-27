abf54a4fd79684d93b80d5cab5ea6068f41469b3
BiaCalifornia13 <bia.sdca16@gmail.com>
2023-05-27 15:50:41 -0300

    Alteracao so no branch da Ana!

--- TRABALHOS/BLC/Program.cs
+++ TRABALHOS/BLC/Program.cs
@@ -10,3 +10,14 @@ Console.WriteLine(new string('*', 10));    // **********
 char[] ca = "Hello".ToCharArray();
 string s = new string(ca);                 // Reconstrução de string a partir de vetor de chars
 Console.WriteLine(s);                      // "Hello"
+
+string empty = "";
+Console.WriteLine(empty == "");                        // True
+Console.WriteLine(empty == string.Empty);              // True
+Console.WriteLine(empty.Length == 0);                  // True
+
+string nullString = null;
+Console.WriteLine(nullString == null);                 // True
+Console.WriteLine(nullString == "");                   // False
+Console.WriteLine(string.IsNullOrEmpty (nullString));  // True
+Console.WriteLine(nullString.Length == 0);             // NullReferenceException
